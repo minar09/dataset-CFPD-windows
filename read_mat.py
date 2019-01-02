f1e22eecf506b7f7b63599993c545959441f56bb
Matiur Rahman Minar <minar09.bd@gmail.com>
2019-01-02 16:34:01 +0900

    read data

--- read_mat.py
+++ read_mat.py
@@ -1,4 +1,6 @@
 import h5py
+import numpy as np
+
 with h5py.File('fashon_parsing_data.mat', 'r') as file:
     print(list(file.keys()))
     # print(file)
@@ -6,6 +8,11 @@ with h5py.File('fashon_parsing_data.mat', 'r') as file:
 
     for each in file.keys():
         try:
+            
+            data = file.get(each) # Get a certain dataset
+            data = np.array(data)
+            print(data)
+        
             print(file[each])
             print(file[each][0])
             print(file[each][0][0])
@@ -18,11 +25,23 @@ with h5py.File('fashon_parsing_data.mat', 'r') as file:
         except Exception as e:
             print(e)
 
+    data = file.get('fashion_dataset/segmentation') # Get a certain dataset
+    data = np.array(data)
+    print(data)
+    
     print(file['fashion_dataset'])
     print(file['fashion_dataset'][0])
     print(file['fashion_dataset'][0][0])
+    # ['category_label', 'color_label', 'img_name', 'segmentation']
     for every in file['fashion_dataset'][0]:
         st = every
         obj = file[st]
         for i in obj:
             print(i)
+    print(file[file['fashion_dataset'][0][0]])
+    print(file['fashion_dataset'][0, 0])
+    struArray = file['fashion_dataset']
+    print(struArray[0, 0])  # this is the HDF5 reference
+    print(file[struArray[0, 0]]['segmentation'])  # this is the actual data
+    print(file[struArray[0, 0]]['segmentation'][0])  # this is the actual data
+    print(file[struArray[0, 0]]['segmentation'].value)  # this is the actual data
